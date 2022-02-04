1de0770943be705f8e54527c9c0d8b0d82b96af9
Gysen Lionel <gysenlionel@gmail.com>
2022-02-04 12:28:09 +0100

    devtools

--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,6 +1,6 @@
 import { createStore } from 'redux'
 import rootReducer from './reducer/rootReducer'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from '@redux-devtools/extension'
 
 const store = createStore(rootReducer, composeWithDevTools())
 
